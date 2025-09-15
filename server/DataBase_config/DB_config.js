cdf943b2fd693d1ee5f707af11f35512e6f30045
renad <120721701+Renadyousef@users.noreply.github.com>
2025-09-15 19:54:12 +0300

    connected DB

---
+++ server/DataBase_config/DB_config.js
@@ -0,0 +1,9 @@
+//DB_config file
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: 'postgresql://postgres:Ela@911911@db.rezcixrdprtvpfkvbaxw.supabase.co:5432/postgres',
+  ssl: { rejectUnauthorized: false }, // needed for Supabase
+});
+
+module.exports = pool;
--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const pool=require('../DataBase_config/DB_config')
 const express = require('express');
 const app = express();
 
@@ -5,6 +6,12 @@ const app = express();
 app.use(express.json());
 
 // use routes here
+
+//testing DB connection
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) console.error(err);
+  else console.log('Connected to database at', res.rows[0].now);
+});
 
 
 module.exports = app;
