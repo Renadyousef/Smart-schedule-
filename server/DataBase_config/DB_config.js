c543e72bb733c534673f27243ea0f77cf061656c
renad <120721701+Renadyousef@users.noreply.github.com>
2025-10-18 11:19:08 +0300

    Update DB_config.js

--- server/DataBase_config/DB_config.js
+++ server/DataBase_config/DB_config.js
@@ -3,9 +3,9 @@ const { Pool } = pkg;
 import 'dotenv/config';
 
 const pool = new Pool({
-  connectionString:process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // needed for Supabase
- 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  host: "db.rezcixrdprtvpfkvbaxw.supabase.co", // ensure host
+  port: 5432,
+  family: 4, // force IPv4
 });
-
-export default pool;
