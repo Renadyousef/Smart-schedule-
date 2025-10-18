91122054e3e5d4e49c0a885b27b4b7d8fe88985c
renad <120721701+Renadyousef@users.noreply.github.com>
2025-10-18 10:36:07 +0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,4 @@
 // src/index.js
 import app from './app.js'; // use import and include .js
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
