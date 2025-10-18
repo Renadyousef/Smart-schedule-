1b8d299c06ae8db128f0f7e560e4404f4b0e21a6
renad <120721701+Renadyousef@users.noreply.github.com>
2025-10-18 10:41:48 +0300

    Update Departments.jsx

--- client/src/components/Auth/Departments.jsx
+++ client/src/components/Auth/Departments.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import API from "../../api"; 
+import API from "../../API_continer"; 
 
 export default function DepartmentDropdown({ onSelect }) {
   const [departments, setDepartments] = useState([]);
