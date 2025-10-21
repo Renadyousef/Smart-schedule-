04fb4ca1f8c71f6adc65d831f0bc1d2d908d2050
renad <120721701+Renadyousef@users.noreply.github.com>
2025-10-21 20:44:57 +0300

    in index.js comments for ur docker

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,16 @@
 // src/index.js
 import app from './app.js'; // use import and include .js
 
-// const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-//for docker testing
+//app.listen(PORT, () => {
+ // console.log(`Server running on port ${PORT}`);
+//});
+//********for docker testing : uncoment above from line 4 to 8
+//after local comment 4->8
 
 
 
 
 export default app;
-//this is for vercel serveless behaviuor
+//this is for vercel serveless behaviuor:before push uncomment line 15 for vercel
